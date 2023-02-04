d614f4a5fff234fbf9e2556588e902c6b0e6df30
Paul Alonte <paulalonte@Paul-Alonte.local>
2023-02-04 19:56:32 +0100

    setup react playground

---
+++ playgrounds/react/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div
+      style={{
+        width: "400px",
+        height: "400px",
+        // backgroundColor: "var(--sd-lo-primary-primary900)",
+        backgroundColor: "var(--sd-green-green500)",
+      }}
+    >
+      App
+    </div>
+  );
+};
+
+export default App;
