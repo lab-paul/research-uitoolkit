29bdf96d6cb696b0af893566e86fabeea0b43586
Paul Alonte <paulalonte@Paul-Alonte.local>
2023-05-05 10:54:31 +0200

    updated appbar

--- playgrounds/react/src/App.tsx
+++ playgrounds/react/src/App.tsx
@@ -1,15 +1,19 @@
 import React from "react";
+import "./App.css";
 
 const App = () => {
   return (
     <div
       style={{
-        width: "400px",
-        height: "400px",
+        color: "white",
+        fontFamily: "sans-serif",
         backgroundColor: "var(--sd-lo-primary-primary900)",
+        display: "flex",
+        alignItems: "center",
+        padding: "2em",
       }}
     >
-      App
+      <h3 style={{ fontWeight: "100" }}>AppBar</h3>
     </div>
   );
 };
